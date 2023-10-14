8c629e9ffc668524ebf23ffb6e9f42df1952f162
JS <jseb.minet@gmail.com>
2023-10-14 22:34:34 +0200

    add connector registration

--- app/debezium-connect/connector/register-connectors.sh
+++ app/debezium-connect/connector/register-connectors.sh
@@ -1,3 +1,3 @@
 #bin/bash
-curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://debezium-ui.localtest.me:8083/connectors -d @mongodb-connector.json
-curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://debezium-ui.localtest.me:8083/connectors -d @jdbc-sink.json
+curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://connect.localtest.me/connectors -d @mongodb-connector.json
+curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://connect.localtest.me/connectors -d @jdbc-sink.json
